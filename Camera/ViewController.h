4739171c5b83753675142d1cbe98468971ca8425
Diego Mazzone <mazzone.diego@gmail.com>
2015-05-05 18:11:58 -0300

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Diego Mazzone on 5/5/15.
+//  Copyright (c) 2015 Mundo Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
