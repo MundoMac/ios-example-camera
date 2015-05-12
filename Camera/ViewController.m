66d93bd2d4878deeed04c42947d9783d79796ab5
Diego Mazzone <mazzone.diego@gmail.com>
2015-05-12 20:10:03 -0300

    Initial commit

--- Camera/ViewController.m
+++ Camera/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,12 +18,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.imageView.contentMode = UIViewContentModeScaleAspectFill;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)changeImage:(UITapGestureRecognizer *)sender
+{
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil
+                                                             delegate:self
+                                                    cancelButtonTitle:@"Cancelar"
+                                               destructiveButtonTitle:@"Eliminar"
+                                                    otherButtonTitles:@"Escoger foto existente", @"Tomar una foto", nil];
+    
+    [actionSheet showInView:self.view];
 }
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+    UIImagePickerController *imagePicker = [UIImagePickerController new];
+    imagePicker.delegate = self;
+    
+    if ([[actionSheet buttonTitleAtIndex:buttonIndex] isEqualToString:@"Escoger foto existente"]) {
+
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+        
+    } else if ([[actionSheet buttonTitleAtIndex:buttonIndex] isEqualToString:@"Tomar una foto"]) {
+        
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+        
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+    self.imageView.image = chosenImage;
+    self.imageView.layer.cornerRadius = self.imageView.frame.size.width / 2;
+    self.imageView.clipsToBounds = YES;
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
